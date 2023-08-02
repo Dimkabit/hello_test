590cf6f57e9fac45eecb385ce255c98209b2c779
Dmitry <65062995+Dimkabit@users.noreply.github.com>
2023-08-02 13:55:04 +0300

    Update action_ajax_form.php

--- php/action_ajax_form.php
+++ php/action_ajax_form.php
@@ -1,11 +1,11 @@
 <?php
-if (isset($_POST["email"]) && isset($_POST["text"])) { 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["email"]) && isset($_POST["text"])) { 
     // Sanitize and validate the input
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
     $text = filter_input(INPUT_POST, 'text', FILTER_SANITIZE_STRING);
 
-    // Form data processing
-    // ...
+    // Form data processing (произведите обработку данных формы по вашим требованиям)
+    // Например, можно отправить письмо или записать данные в базу данных
 
     // Form a response array
     $result = array(
@@ -13,7 +13,8 @@ if (isset($_POST["email"]) && isset($_POST["text"])) {
         'text' => $text
     );
 
-    // Convert the response array to JSON
+    // Convert the response array to JSON and send it back to the client
+    header('Content-Type: application/json');
     echo json_encode($result);
 }
 ?>
