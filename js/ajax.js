9556a697537e90c36829ad756f7473f1b099adb7
Dmitry <65062995+Dimkabit@users.noreply.github.com>
2023-08-02 13:56:44 +0300

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,25 +1,25 @@
-/* Article FructCode.com */
-$( document ).ready(function() {
-   $("#btn").click(
-     function(){
-        sendAjaxForm('result_form', 'ajax_form', 'php/action_ajax_form.php');
-        return false; 
-     }
-  );
-});
+  $(document).ready(function() {
+    $("#btn").click(function() {
+      sendAjaxForm();
+    });
+  });
 
-function sendAjaxForm(result_form, ajax_form, url) {
-   $.ajax({
-       url:     'php/action_ajax_form.php', //url страницы (action_ajax_form.php)
-       type:     "POST", //метод отправки
-       dataType: "html", //формат данных
-       data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-       success: function(response) { //Данные отправлены успешно
-          result = $.parseJSON(response);
-          $('#result_form').html('E-mail: '+result.name+'<br>text: '+result.phonenumber);
+  function sendAjaxForm() {
+    // Получаем данные из формы
+    var formData = $("#ajax_form").serialize();
+
+    $.ajax({
+      url: 'php/action_ajax_form.php', // URL страницы (action_ajax_form.php)
+      type: 'POST', // Метод отправки
+      dataType: 'json', // Формат данных - ожидаем JSON от сервера
+      data: formData,
+      success: function(response) {
+        // Данные отправлены успешно
+        $('#result_form').html('E-mail: ' + response.email + '<br>Text: ' + response.text);
       },
-      error: function(response) { // Данные не отправлены
-           $('#result_form').html('Ошибка. Данные не отправлены.');
+      error: function() {
+        // Данные не отправлены
+        $('#result_form').html('Ошибка. Данные не отправлены.');
       }
-   });
-}
+    });
+  }
